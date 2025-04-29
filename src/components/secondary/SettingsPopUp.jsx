676d6f899113119a6a39b15299e34cac89cde985
alzatin <47953734+alzatin@users.noreply.github.com>
2025-04-29 16:19:47 -0400

    in progress new settings

--- src/components/secondary/SettingsPopUp.jsx
+++ src/components/secondary/SettingsPopUp.jsx
@@ -3,6 +3,13 @@ import Globalvariables from "../../js/globalvariables.js";
 import CreatableSelect from "react-select/creatable";
 import topics from "../../js/maslowTopics.js";
 import { use } from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import Switch from "@mui/material/Switch";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Slider from "@mui/material/Slider";
 
 const SettingsPopUp = ({ setSettingsPopUp, shortCutsOn, setShortCuts }) => {
   let repoTopics = [];
@@ -12,24 +19,19 @@ const SettingsPopUp = ({ setSettingsPopUp, shortCutsOn, setShortCuts }) => {
     });
   }
 
-  //const projectRef = useRef(Globalvariables.currentRepo.repoName);
   const projectTopicRef = useRef(repoTopics);
   const projectDescriptionRef = useRef(Globalvariables.currentRepo.description);
-  //const projectLicenseRef = useRef();
   const projectUnitsRef = useRef(Globalvariables.topLevelMolecule.unitsKey);
   const shortcutsRef = useRef(shortCutsOn);
 
   const dateString = Globalvariables.currentRepo.dateCreated;
   const dateCreated = new Date(dateString);
 
-  /* Handles form submission for create new/ export project form */
   const handleSubmit = async (e) => {
     e.preventDefault();
     setSettingsPopUp(false);
-    //const projectName = projectRef.current.value; add tooltip directing user to github to change project name
     const projectTopicArray = projectTopicRef.current.getValue();
     const projectTopic = [];
-    //const projectLicense = projectLicenseRef.current.value;
     projectTopicArray.forEach((topic) => {
       projectTopic.push(topic[`value`]);
     });
@@ -39,6 +41,57 @@ const SettingsPopUp = ({ setSettingsPopUp, shortCutsOn, setShortCuts }) => {
     Globalvariables.currentRepo.topics = projectTopic;
 
     setShortCuts(shortcutsRef.current.checked);
+  };
+  const [value, setValue] = React.useState(0);
+
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      "aria-controls": `simple-tabpanel-${index}`,
+    };
+  }
+  function CustomTabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+      </div>
+    );
+  }
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const [state, setState] = React.useState({
+    shortcut: true,
+    displaytheme: false,
+    fontSize: 12,
+  });
+
+  const handleCheckChange = (event) => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.checked,
+    });
+    if (event.target.name === "shortcut") {
+      setShortCuts(event.target.checked);
+    }
+
+    if (event.target.name === "fontSize") {
+      console.log(event.target.value);
+      setState({
+        ...state,
+        fontSize: event.target.value,
+      });
+    }
   };
 
   return (
@@ -52,75 +105,111 @@ const SettingsPopUp = ({ setSettingsPopUp, shortCutsOn, setShortCuts }) => {
         >
           {"\u00D7"}
         </a>
-        <h2 style={{ margin: "0 0 15px 0" }}>Project Preferences</h2>
-        <div id="project-info">
-          <div id="project-info-name">
-            <label>Project Name</label>
-
-            <p title="To change the Project Name go to your Github repository">
-              {Globalvariables.currentRepo.repoName}
-            </p>
-          </div>
-          <div id="project-info-date">
-            <label>Date Created</label>
-            <p>{dateCreated.toDateString()}</p>
-          </div>
-        </div>
-
+        <h2 style={{ margin: "0 0 15px 0" }}> Project Preferences</h2>
         <form
           className="settings-form project-info"
           onSubmit={(e) => {
             handleSubmit(e);
           }}
         >
-          {/*Project name change?? */}
-          <label htmlFor="project-description">Project Description</label>
-          <input
-            defaultValue={Globalvariables.currentRepo.description}
-            ref={projectDescriptionRef}
-          />
-          <label htmlFor="project-topics">Project Tags</label>
-          <CreatableSelect
-            defaultValue={repoTopics}
-            isMulti
-            name="Project Topics"
-            options={topics}
-            className="basic-multi-select"
-            classNamePrefix="select"
-            ref={projectTopicRef}
-          />
-          <label htmlFor="measure-units">Units</label>
-          <select
-            id="measure-units"
-            defaultValue={Globalvariables.topLevelMolecule.unitsKey}
-            ref={projectUnitsRef}
-          >
-            <option key={"inchesop"} value={"Inches"}>
-              Inches
-            </option>
-            <option key={"millop"} value={"MM"}>
-              MM
-            </option>
-          </select>
-          <div>
-            <label htmlFor="shortcut-button">Show/Hide Shortcuts</label>
-            {/*toggle between displaying ghost shortcuts and not */}
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              aria-label="basic tabs example"
+            >
+              <Tab label="Project Information" {...a11yProps(0)} />
+              <Tab label="Canvas Settings" {...a11yProps(1)} />
+              <Tab label="Project Settings" {...a11yProps(2)} />
+            </Tabs>
+          </Box>
+          <CustomTabPanel value={value} index={0}>
+            <div id="project-info">
+              <div id="project-info-name">
+                <label>Project Name</label>
+
+                <p title="To change the Project Name go to your Github repository">
+                  {Globalvariables.currentRepo.repoName}
+                </p>
+              </div>
+              <div id="project-info-date">
+                <label>Date Created</label>
+                <p>{dateCreated.toDateString()}</p>
+              </div>
+            </div>
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={1}>
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Switch
+                    checked={state.shortcut}
+                    onChange={handleCheckChange}
+                    name="shortcut"
+                  />
+                }
+                label="Shortcut Helper Show/Hide"
+              />
+              <FormControlLabel
+                control={
+                  <Switch
+                    checked={state.displaytheme}
+                    onChange={handleCheckChange}
+                    name="displaytheme"
+                  />
+                }
+                label="Display light/dark"
+              />
+              <FormControlLabel
+                control={
+                  <Slider
+                    aria-label="fontSize"
+                    value={state.fontSize}
+                    onChange={handleCheckChange}
+                    name="fontSize"
+                  />
+                }
+                label="Font Size"
+              />
+            </FormGroup>
+            <label htmlFor="theme-toggle">Display Theme (placeholder)</label>
             <input
               type="checkbox"
-              className="checkbox shortcut-button"
-              name={"shortcut-button"}
-              id={"shortcut-button"}
-              defaultChecked={shortCutsOn}
-              ref={shortcutsRef}
+              className="checkbox "
+              name={"theme-toggle"}
+              id={"theme-toggle-light"}
             />
-          </div>
-          <label htmlFor="theme-toggle">Display Theme (placeholder)</label>
-          <input
-            type="checkbox"
-            className="checkbox "
-            name={"theme-toggle"}
-            id={"theme-toggle-light"}
-          />
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={2}>
+            <label htmlFor="project-description">Project Description</label>
+            <input
+              defaultValue={Globalvariables.currentRepo.description}
+              ref={projectDescriptionRef}
+            />
+            <label htmlFor="project-topics">Project Tags</label>
+            <CreatableSelect
+              defaultValue={repoTopics}
+              isMulti
+              name="Project Topics"
+              options={topics}
+              className="basic-multi-select"
+              classNamePrefix="select"
+              ref={projectTopicRef}
+            />
+            <label htmlFor="measure-units">Units</label>
+            <select
+              id="measure-units"
+              defaultValue={Globalvariables.topLevelMolecule.unitsKey}
+              ref={projectUnitsRef}
+            >
+              <option key={"inchesop"} value={"Inches"}>
+                Inches
+              </option>
+              <option key={"millop"} value={"MM"}>
+                MM
+              </option>
+            </select>
+          </CustomTabPanel>
 
           <button className="submit-button" type="submit">
             Save Changes
