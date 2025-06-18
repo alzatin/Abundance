517dedcff864f330cd4177609badca2e01dbc320
alzatin <47953734+alzatin@users.noreply.github.com>
2025-06-18 17:42:55 -0400

    no nodes

--- src/molecules/molecule.js
+++ src/molecules/molecule.js
@@ -936,12 +936,6 @@ export default class Molecule extends Atom {
               }
             });
           }
-
-          // Add the atom to the list to display
-          this.nodesOnTheScreen.push(atom);
-          // fakes a click on newly placed atom
-          //atom.selected = false;
-
           if (unlock) {
             //Make this molecule spawn with all of it's parent's inputs
             if (atom.atomType == "Molecule") {
