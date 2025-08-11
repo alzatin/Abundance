8e6fe54c94c3a7ec310469bca1463701cf8a92ef
alzatin <47953734+alzatin@users.noreply.github.com>
2025-08-11 15:39:06 -0400

    clean up

--- Lambda AWS Functions/proxyApi/index.js
+++ Lambda AWS Functions/proxyApi/index.js
@@ -9,28 +9,11 @@ import serverless from "serverless-http";
 import jwt from "express-jwt";
 import jwksRsa from "jwks-rsa";
 
-const domain = "dev-ln37eaqfk7dp2480.us.auth0.com";
-const audience = "https://api.github.com/";
-
-const checkJwt = jwt({
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://${domain}/.well-known/jwks.json`,
-  }),
-
-  audience: audience,
-  issuer: `https://${domain}/`,
-  algorithms: ["RS256"],
-});
-
 /**
  * App Variables
  */
 
 const app = express();
-//const port = process.env.SERVER_PORT;
 
 /**
  *  App Configuration
@@ -38,24 +21,6 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/api/greet", checkJwt, async (req, res) => {
-  try {
-    const userID = req.user.sub;
-    const token = await fetchManagementApi();
-    const gitToken = await forwardRequest(token, userID);
-    // Set multiple headers
-    res.set({
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      Allow: "GET, OPTIONS, POST",
-      "Access-Control-Allow-Methods": "GET, OPTIONS, POST",
-      "Access-Control-Allow-Headers": "*",
-    });
-    res.status(200).send({ success: true, message: gitToken });
-  } catch (error) {
-    res.status(500).send({ success: false, message: error.message });
-  }
-});
 app.get("/api/ourAutho", async (req, res) => {
   try {
     const code = req.query.code; // Retrieve 'code' from query parameters
@@ -113,7 +78,7 @@ app.get("/api/mobAutho", async (req, res) => {
           client_id: "Ov23lioNfq4Q063COhYR",
           client_secret: process.env.CLIENT_SECRET_GIT_MOB,
           code: code,
-          redirect_uri: `http://192.168.1.169:4444/callback`,
+          redirect_uri: `${process.env.MOB_TEST_NETWORK}callback`,
         },
         headers: {
           Accept: "application/json",
@@ -181,61 +146,9 @@ app.get("/api/test", (req, res) => {
   res.status(200).send({ success: true, message: code });
 });
 
-const forwardRequest = async (token, userID) => {
-  const options = {
-    method: "GET",
-    url: `https://dev-ln37eaqfk7dp2480.us.auth0.com/api/v2/users/${userID}`,
-    headers: { Authorization: `Bearer ${token}` },
-  };
-
-  try {
-    const response = await axios.request(options);
-    let gitToken = response.data.identities[0].access_token;
-    return gitToken;
-  } catch (error) {
-    throw new Error("Failed to fetch gitToken");
-  }
-};
-
-const fetchManagementApi = async () => {
-  const url = "https://dev-ln37eaqfk7dp2480.us.auth0.com/oauth/token";
-  const headers = {
-    "Content-Type": "application/json",
-  };
-
-  const body = JSON.stringify({
-    client_id: "XWdAtXHXzzoIzbAj39I9ffebVfJWxpx4",
-    client_secret: process.env.CLIENT_SECRET,
-    audience: "https://dev-ln37eaqfk7dp2480.us.auth0.com/api/v2/",
-    grant_type: "client_credentials",
-  });
-
-  try {
-    const response = await axios.post(url, body, { headers });
-    if (response.status !== 200) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return response.data.access_token;
-  } catch (error) {
-    console.error(error);
-    throw new Error("Failed to fetch management API token");
-  }
-};
-
-// Uncomment and use the apiRouter if needed
-// app.use("/api", apiRouter);
-// apiRouter.use("/messages", messagesRouter);
-
 app.use(function (err, req, res, next) {
   console.log(err);
   res.status(500).send(err.message);
 });
-
-/**
- * Server Activation
-
-app.listen(serverPort, () =>
-  console.log(`API Server listening on port ${serverPort}`)
-); */
 
 export const handler = serverless(app);
