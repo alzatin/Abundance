e139a64738385b28d1b3a6048368c02dd2c61ff6
alzatin <47953734+alzatin@users.noreply.github.com>
2025-10-06 10:12:33 -0400

    center axis helper, center, updated gizmo

--- src/components/render/ThreeControls.jsx
+++ src/components/render/ThreeControls.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { OrbitControls, GizmoHelper, GizmoViewport } from "@react-three/drei";
+import * as THREE from "three";
 
 const Controls = React.memo(
   React.forwardRef(function Controls(
@@ -14,21 +15,24 @@ const Controls = React.memo(
           zoomSpeed={0.5}
           enableDamping={enableDamping}
         />
+
+        {/* Mark the origin with a small sphere */}
+        <mesh position={[0, 0, 0]}>
+          <sphereGeometry args={[0.1, 32, 32]} />
+          <meshBasicMaterial color="gray" />
+        </mesh>
+
         {axesParam && (
-          <GizmoHelper
-            alignment={"bottom-right"}
-            margin={window.innerWidth < 768 ? [50, 50] : [70, 100]}
-            onTarget={() => {
-              return controlsRef?.current?.target;
-            }}
-            onUpdate={() => {
-              controlsRef.current?.update();
-            }}
-          >
-            <mesh scale={window.innerWidth < 768 ? 0.5 : 1}>
-              <GizmoViewport font="18px Inter var, HKGrotesk, sans-serif" />
-            </mesh>
-          </GizmoHelper>
+          <>
+            <GizmoHelper alignment="bottom-right" margin={[80, 100]}>
+              <GizmoViewport
+                axisColors={["#9d4b4b", "#2f7f4f", "#3b5b9d"]}
+                labelColor="white"
+              />
+            </GizmoHelper>
+
+            <primitive object={new THREE.AxesHelper(300)} />
+          </>
         )}
       </>
     );
