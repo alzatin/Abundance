b8b461703d975c93821fb9fc8881bd0a072cb6ca
alzatin <47953734+alzatin@users.noreply.github.com>
2026-01-20 15:18:40 -0500

    Update SimpleControlPanel.jsx

--- src/components/secondary/SimpleControlPanel.jsx
+++ src/components/secondary/SimpleControlPanel.jsx
@@ -471,7 +471,7 @@ export const SimpleControlPanel = forwardRef(function SimpleControlPanel(
             justifyContent: "center",
           }}
           onClick={() => (setCollapsed(false), setContentCollapsed())}
-          title={title}
+          title={`Open ${title}`}
         >
           {React.createElement(collapsedIcon, { size: 18 })}
         </div>
