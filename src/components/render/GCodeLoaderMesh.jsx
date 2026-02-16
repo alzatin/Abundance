15364eb46000e7dd47adc71cb0ed73d6a453fd8a
alzatin <47953734+alzatin@users.noreply.github.com>
2026-02-16 14:25:44 -0500

    loading local loader

--- src/components/render/GCodeLoaderMesh.jsx
+++ src/components/render/GCodeLoaderMesh.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useRendering } from "../../contexts/RenderingContext.jsx";
-
-import { GCodeLoader } from "three/addons/loaders/GCodeLoader.js";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 import { Group } from "three";
+//Loading a local version of the three gcode loader for color and other mods
+import { GCodeLoader } from "../../js/GCodeAbundanceLoader.js";
 
 export default function GCodeLoaderMesh({ authorizedUserOcto }) {
   const { gcodeParts } = useRendering(); // Access mesh state from context
@@ -39,6 +41,15 @@ export default function GCodeLoaderMesh({ authorizedUserOcto }) {
   if (object) {
     object.rotation.x = Math.PI / 2; // Rotate to lay flat on XY plane
   }
-
+  /*const myMesh = React.useRef();
+  useFrame(({ clock }) => {
+    myMesh.current.rotation.x = clock.elapsedTime;
+  });
+  return (
+    <mesh ref={myMesh}>
+      <boxGeometry />
+      <meshBasicMaterial color="royalblue" />
+    </mesh>
+  );*/
   return object ? <primitive object={object} /> : null;
 }
