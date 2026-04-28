24aacf3ea33afd42f27041f0d10a724679e97524
alzatin <47953734+alzatin@users.noreply.github.com>
2026-04-28 17:54:59 -0400

    reset waiting force

--- src/molecules/molecule.js
+++ src/molecules/molecule.js
@@ -1018,6 +1018,7 @@ export default class Molecule extends Atom {
       // withheld. Only call setReady if we have a valid value.
       const value = this.value;
 
+      this.setWaiting();
       if (value !== null && value !== undefined) {
         this.setReady(value);
       }
